2b6262559f10dd5012befd1abe5aca6085f0d8e1
A7t5i0 <84842351+A7t5i0@users.noreply.github.com>
2021-08-19 18:15:17 +0200

    Add files via upload

---
+++ bat.rb
@@ -0,0 +1,21 @@
+def prep
+  prepcommands = ['sudo pacman -Syyu --noconfirm', 'sudo curl -O https://blackarch.org/strap.sh > strap.sh', 'sudo chmod +x strap.sh', 'sudo ./strap.sh']
+  for prepcommand in prepcommands
+    puts '>>>COMMAND IN PROGRESS:' + prepcommand
+    exec prepcommand
+    puts '>>>COMMAND COMPLETE!'
+  end
+end
+
+def install
+  installcommands = ['webapp', 'fuzzer', 'scanner', 'proxy', 'windows', 'dos', 'disassembler', 'cracker', 'voip', 'exploitation', 'recon', 'spoof', 'forensics', 'crypto', 'backdoor', 'networking', 'misc', 'defensive', 'wireless', 'automation', 'sniffer', 'binary', 'packer', 'reversing', 'mobile', 'malware', 'code-audit', 'social', 'honeypot', 'hardware', 'fingerprint', 'decompiler', 'config', 'debugger', 'firmware', 'bluetooth', 'database', 'automobile', 'nfc', 'tunnel', 'drone', 'unpacker', 'radio', 'keylogger', 'stego', 'scsanner', 'anti-forensics', 'ids', 'gpu']
+  for installcommand in installcommands
+    puts '>>>DOWNLOAD IN PROGRESS' + installcommand
+    exec installcommand
+    puts '>>>DOWNLOAD COMPLETE!'
+  end
+end
+
+puts '>>>CTRL C TO CANCEL<<<'
+prep
+install
